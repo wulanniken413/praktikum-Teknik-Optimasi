d5f435b54332ce14f76f315544a347d0212b3ad5
Wulan 1800018413 <wulanniken13@gmail.com>
2022-01-06 21:40:43 -1100

    prak TO

---
+++ algen_knapsack.php
@@ -0,0 +1,14 @@
+<?php
+
+class Catalouge
+{
+    function product($parameters)
+    $collectionOfListProduct = [];
+
+    $row_data 
+
+}
+$parameters  = [
+    'file_nama' => 'product.txt',
+    'coulumns' => ['item', 'price']
+];
